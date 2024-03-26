c8d5286685df0caa252de7e1538f36c94be33448
Sebastian <selfishworm117@gmail.com>
2024-03-26 14:44:00 +0100

    5rtetrsfsdg<rst

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import IngredientsBox from './components/IngredientsBox'
 import PizzaProvider from './components/PizzaProvider'
-import { Ingredients } from './components/PizzaContext'
 import OrderView from './components/OrderView'
 import Header from './components/Header'
 import Button from './components/Button'
@@ -8,7 +7,6 @@ import Button from './components/Button'
 function App() {
    return (
       <>
-      <Header />
 
       <PizzaProvider>
          <OrderView />
