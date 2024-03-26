e31fafe5319c4c2beae840cb3dab3896287f48cc
Maja Wennstam <majawennstam@bahnhof.se>
2024-03-26 13:03:27 +0100

    Majsan header och knappar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import PizzaProvider from './components/PizzaProvider'
+import Header from './components/Header'
+import Button from './components/Button'
 
 function App() {
    return (
+      <>
+      <Header />
+
       <PizzaProvider>
          <h1>pizza :D</h1>
       </PizzaProvider>
+      <Button />
+      </>
    )
 }
 
