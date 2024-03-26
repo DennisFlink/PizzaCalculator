4e439062ac609c310e4b1aa152a4f21e060c10a2
DennisFlink <dennis.sabriev@outlook.com>
2024-03-26 12:01:10 +0100

    asd

--- src/components/IngridientList.tsx
+++ src/components/IngridientList.tsx
@@ -6,7 +6,7 @@ const IngridientList: React.FC<PROP> = ({ type }) => {
    return (
       <div className="CONTAINER">
          {type.map((ingrident, index) => (
-            <div className="BOX" key={index}>
+            <div className="box" key={index}>
                <input type="checkbox" id={ingrident.name}></input>
                <label htmlFor={ingrident.name}>{ingrident.name}</label>
             </div>
