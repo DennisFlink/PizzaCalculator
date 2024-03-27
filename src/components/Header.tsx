07d8b3cd77c266946255c6dd4b4c35c331eafa8a
Gustav Syrén <g.e.syren@gmail.com>
2024-03-27 10:03:05 +0100

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,21 +21,21 @@ const Header =() =>{
     <div className="headerDefault">
         <h1> Pizzakalkylator</h1>;
             <div className="btnDefault-container">
-                <Button className="button close" label={''} onClick={() => setCartOpen(true)} /> 
+                <Button className="button close" label='' onClick={() => setCartOpen(true)} /> 
             </div>
     </div>)
     :changeOrder ? ( 
     <div className="headerChange">
     {/* <h1> {pizzaData.id} </h1>; */}
         <div className="btnChange-container">
-            <Button className="button close" label={''} onClick={() => setChangeOrder(true)}/>
+            <Button className="button close" label='' onClick={() => setChangeOrder(true)}/>
         </div>
     </div>
 
     ):(
     <div className="headerCart">
         <div className="btnCart-container">
-            <Button className="button cart" label={''} onClick={() => setCartOpen(false)}/>
+            <Button className="button cart" label='' onClick={() => setCartOpen(false)}/>
         </div>
         <h1>Pizzakalkylator</h1>
     </div>) }
