7208144e9eb7617dbc2c5b5169da3ab8102c8066
skoanton <antonhakanssonn@gmail.com>
2024-03-27 01:45:56 +0100

    removed imports thats not needed

--- src/components/OrderView.tsx
+++ src/components/OrderView.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react'
+import { useContext } from 'react'
 import '../sass/components/_orderView.scss'
 import { PizzaContext } from './PizzaContext'
 import { ACTION } from './PizzaProvider'
